b9b935d99f1545a9571823ca4edcf999cc1bb745
arthgirard <mailarthurgirard@gmail.com>
2026-02-16 15:06:36 -0500

    Code cleanup

--- backend/tracker.py
+++ backend/tracker.py
@@ -1,4 +1,3 @@
-
 import time
 import cv2
 import numpy as np
@@ -14,7 +13,6 @@ class ResultStore:
 
     def callback(self, result, output_image, timestamp_ms):
         self.latest = (result, timestamp_ms)
-
     
 class landmark_extract:
     def __init__(self):
@@ -63,74 +61,3 @@ class eye_tracker:
             num_faces=1) 
 
         self.landmarker = vision.FaceLandmarker.create_from_options(options)
-
-
-
-    
-# class App:
-#     def __init__(self, settings):
-#         self.settings = settings # settings est l'objet de eye_tracker
-#         self.extractor = landmark_extract()
-#     def run(self): 
-
-#         cap = cv2.VideoCapture(0)
-
-#         t0 = time.monotonic()
-
-#         while True:
-#             ret, frame_cv = cap.read()   # ret = success flag, frame = image
-#             # si ret(success flag) ne foncitonne pas, arreter le loop
-#             if not ret:
-#                 break
-
-#             # Load the frame rate of the video using OpenCV’s CV_CAP_PROP_FPS
-#             # fps = cap.get(cv2.CAP_PROP_FPS)
-#             # print("FPS:", fps)
-
-    
-#             # You’ll need it to calculate the timestamp for each frame. On utilise la librairie time et on creer un timestamp pour chaque millisecondes
-#             self.timestamps = int((time.monotonic()-t0) * 1000)
-    
-#             # Convert the frame received from OpenCV to a MediaPipe’s Image object.
-#             frame_rgb = cv2.cvtColor(frame_cv, cv2.COLOR_BGR2RGB)
-#             mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb)
-
-#             # Send live image data to perform face landmarking.
-#             # The results are accessible via the `result_callback` provided in
-#             # the `FaceLandmarkerOptions` object.
-#             # The face landmarker must be created with the live stream mode.
-#             self.settings.landmarker.detect_async(mp_image, self.timestamps)
-
-#             latest = self.settings.store.latest
-#             if latest is not None:
-#                 result, ts = latest
-#                 if result.face_landmarks:
-#                         h, w = frame_cv.shape[:2]
-#                         landmarks = result.face_landmarks[0]
-
-#                         left_eye, right_eye = self.extractor.extract_data(landmarks, h, w)
-
-#                         # draw every eye
-#                         for point in left_eye:
-#                             xl, yl = point["pixel"]
-#                             cv2.circle(frame_cv, (xl, yl), 2, (0, 255, 0), -1)
-
-#                         for point in right_eye:
-#                             xr, yr = point["pixel"]
-#                             cv2.circle(frame_cv, (xr, yr), 2, (0, 255, 0), -1)
-
-#                         print(f"left: {xl}, {yl} right: {xr}, {yr}")
-
-
-#             cv2.imshow("Webcam", frame_cv) #montre la video  frame== cap.read()
-#             if cv2.waitKey(1) & 0xFF == ord('q'): #pour quitter, appuyer sur "q"
-#                 break
-
-
-
-# def main():
-#     instance = App(eye_tracker(model_path, 1))
-#     instance.run()
-    
-# if __name__ == "__main__":
-#     main()
